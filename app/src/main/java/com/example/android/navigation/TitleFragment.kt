d0929bf33231b61fd842aaeadfeec3f6972ea182
Herve Desrosiers <hdesrosiers@stingray.com>
2020-08-06 14:19:57 -0400

    step 2: Navigation component graph resource, NavHostFragment, Added fragments to Navigation Graph, connected fragments with action, onClickListener, Navigate with action

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -14,6 +18,13 @@ class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false)
+        val layout: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+        layout.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)) //version 3
+//        layout.playButton.setOnClickListener { view: View -> //anonymous onClick function
+//            // Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment) //version 1
+//            // view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment) //version 2
+//        }
+
+        return layout.root
     }
 }
