ce7c2becceb8f0abea14d80328c7b372d94da79e
Herve Desrosiers <hdesrosiers@stingray.com>
2020-08-07 14:29:06 -0400

    step 7: added safe-args dependency to use args from the bundle and replaced navigation fragment actions with NavDirections

--- app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -33,7 +33,7 @@ class GameOverFragment : Fragment() {
         val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_over, container, false)
         binding.tryAgainButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_gameOverFragment_to_titleFragment)
+            view.findNavController().navigate(GameOverFragmentDirections.actionGameOverFragmentToTitleFragment())
         }
         return binding.root
     }
--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -18,11 +18,11 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val layout: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
-        layout.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)) //version 3
-//        layout.playButton.setOnClickListener { view: View -> //anonymous onClick function
-//            // Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment) //version 1
-//            // view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment) //version 2
-//        }
+//        layout.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)) //version 3
+        layout.playButton.setOnClickListener { view: View -> //anonymous onClick function
+            // Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment) //version 1
+             view.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment()) //version 2
+        }
 
         setHasOptionsMenu(true)
         return layout.root
