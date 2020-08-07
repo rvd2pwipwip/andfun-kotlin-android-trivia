d2ec3d2a0fca6e056143fc4eebb5ecb59f4cef4e
Herve Desrosiers <hdesrosiers@stingray.com>
2020-08-07 13:37:31 -0400

    step 6: added appbar menu and fixed fragment labels (for appbar titles)

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,13 +1,12 @@
 package com.example.android.navigation
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -25,6 +24,16 @@ class TitleFragment : Fragment() {
 //            // view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment) //version 2
 //        }
 
+        setHasOptionsMenu(true)
         return layout.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController()) || super.onOptionsItemSelected(item)
     }
 }
